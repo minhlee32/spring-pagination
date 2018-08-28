33829f5061487662daa16f1177e40e277d7e63c4
Minh Lee <minhlee.fre.mta@gmail.com>
2018-08-28 15:33:24 +0700

    refactor code CustomerRepository

--- src/main/java/com/codegym/cms/repositories/CustomerRepository.java
+++ src/main/java/com/codegym/cms/repositories/CustomerRepository.java
@@ -1,6 +1,7 @@
 package com.codegym.cms.repositories;
 
 import com.codegym.cms.models.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CustomerRepository extends Repository<Customer> {
+public interface CustomerRepository extends PagingAndSortingRepository<Customer,Long> {
 }
