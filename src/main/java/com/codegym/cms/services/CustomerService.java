1de7acf19b099e9f7ccd5b51e2e72a4318577732
Minh Lee <minhlee.fre.mta@gmail.com>
2018-08-28 11:32:55 +0700

    create Customer Service interface

---
+++ src/main/java/com/codegym/cms/services/CustomerService.java
@@ -0,0 +1,15 @@
+package com.codegym.cms.services;
+
+import com.codegym.cms.models.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> findAll();
+
+    Customer findById(Long id);
+
+    void save(Customer customer);
+
+    void remove(Long id);
+}
