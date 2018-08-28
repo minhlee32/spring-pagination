93c5688b7d63ac4de4316d0f1eb7e3c331b3913c
Minh Lee <minhlee.fre.mta@gmail.com>
2018-08-28 16:00:17 +0700

    create interface ProvinceRepository

---
+++ src/main/java/com/codegym/cms/repositories/ProvinceRepository.java
@@ -0,0 +1,7 @@
+package com.codegym.cms.repositories;
+
+import com.codegym.cms.models.Province;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ProvinceRepository extends PagingAndSortingRepository<Province,Long> {
+}
