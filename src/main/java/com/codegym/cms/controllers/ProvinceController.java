c960bd24dcfa6a13947dd0c637af2f647502ccdc
Minh Lee <minhlee.fre.mta@gmail.com>
2018-08-28 16:18:53 +0700

    refactor code

--- src/main/java/com/codegym/cms/controllers/ProvinceController.java
+++ src/main/java/com/codegym/cms/controllers/ProvinceController.java
@@ -2,7 +2,6 @@ package com.codegym.cms.controllers;
 
 import com.codegym.cms.models.Province;
 import com.codegym.cms.services.ProvinceService;
-import com.codegym.cms.services.ProvinceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
