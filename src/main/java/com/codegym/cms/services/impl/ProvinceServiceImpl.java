1680a014933d6bfdb391c93f25c99e1d8b632fc4
Minh Lee <minhlee.fre.mta@gmail.com>
2018-08-28 16:04:08 +0700

    create class implements interface ProvinceServiceImpl

---
+++ src/main/java/com/codegym/cms/services/impl/ProvinceServiceImpl.java
@@ -0,0 +1,32 @@
+package com.codegym.cms.services.impl;
+
+import com.codegym.cms.models.Province;
+import com.codegym.cms.repositories.ProvinceRepository;
+import com.codegym.cms.services.ProvinceService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ProvinceServiceImpl implements ProvinceService {
+
+    @Autowired
+    private ProvinceRepository provinceRepository;
+
+    @Override
+    public Iterable<Province> findAll() {
+        return provinceRepository.findAll();
+    }
+
+    @Override
+    public Province findById(Long id) {
+        return provinceRepository.findOne(id);
+    }
+
+    @Override
+    public void save(Province province) {
+        provinceRepository.save(province);
+    }
+
+    @Override
+    public void remove(Long id) {
+        provinceRepository.delete(id);
+    }
+}
